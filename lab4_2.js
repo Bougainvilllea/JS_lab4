5866d494a30fa3d914c8a4a2b6bdf38c71f1a00b
Bougainvilllea <80826067+Bougainvilllea@users.noreply.github.com>
2025-06-05 11:25:45 +0700

    Update lab4_2.js

--- lab4_2.js
+++ lab4_2.js
@@ -1,10 +1,15 @@
+// Объявляем функцию readConfig, которая принимает имя конфига и callback-функцию
 function readConfig (name, callback) {
+    // Имитируем асинхронную операцию с помощью setTimeout
     setTimeout(() => {
+        // Выводим сообщение о загрузке конфига
         console.log('(1) config from ' + name + ' loaded')
+        // Вызываем callback-функцию после завершения
         callback()
-    }, Math.floor(Math.random() * 1000))
+    }, Math.floor(Math.random() * 1000)) // Случайная задержка от 0 до 999 мс
 }
 
+// Функция для выполнения SQL-запроса (аналогичная структура)
 function doQuery (statement, callback) {
     setTimeout(() => {
         console.log('(2) SQL query executed: ' + statement)
@@ -12,6 +17,7 @@ function doQuery (statement, callback) {
     }, Math.floor(Math.random() * 1000))
 }
 
+// Функция для HTTP-запроса
 function httpGet (url, callback) {
     setTimeout(() => {
         console.log('(3) Page retrieved: ' + url)
@@ -19,6 +25,7 @@ function httpGet (url, callback) {
     }, Math.floor(Math.random() * 1000))
 }
 
+// Функция для чтения файла
 function readFile (path, callback) {
     setTimeout(() => {
         console.log('(4) Readme file from ' + path + ' loaded')
@@ -26,42 +33,58 @@ function readFile (path, callback) {
     }, Math.floor(Math.random() * 1000))
 }
 
+// Финальная callback-функция
 function callback () {
     console.log('It is done!')
 }
 
+// Начало выполнения программы
 console.log('start');
 
+// Вызываем первую асинхронную функцию readConfig
 readConfig("myConfig", function() {
+    // После ее завершения вызываем doQuery
     doQuery('select * from cities', function() {
+        // После запроса делаем HTTP-запрос
         httpGet('http://google.com', function() {
+            // Затем читаем файл
             readFile('README.md', function() {
+                // В конце вызываем финальный callback
                 callback();
+                // И выводим сообщение о завершении
                 console.log('end');
             });
         });
     });
 });
 
-// console.log('start');
+// Альтернативный вариант (закомментирован) с именованными функциями вместо анонимных
+/*
+console.log('start');
 
-// function notifyReadConfig() {
-//     readConfig("myConfig", notifyDoQuery);
-// }
+// Объявляем функцию-обертку для readConfig
+function notifyReadConfig() {
+    readConfig("myConfig", notifyDoQuery);
+}
 
-// function notifyDoQuery() {
-//     doQuery('select * from cities', notifyHttpGet);
-// }
+// Функция-обертка для doQuery
+function notifyDoQuery() {
+    doQuery('select * from cities', notifyHttpGet);
+}
 
-// function notifyHttpGet() {
-//     httpGet('http://google.com', notifyReadFile);
-// }
+// Функция-обертка для httpGet
+function notifyHttpGet() {
+    httpGet('http://google.com', notifyReadFile);
+}
 
-// function notifyReadFile() {
-//     readFile('README.md', function() {
-//         callback();
-//         console.log('end');
-//     });
-// }
+// Функция-обертка для readFile
+function notifyReadFile() {
+    readFile('README.md', function() {
+        callback();
+        console.log('end');
+    });
+}
 
-// notifyReadConfig();
+// Запускаем цепочку вызовов
+notifyReadConfig();
+*/
